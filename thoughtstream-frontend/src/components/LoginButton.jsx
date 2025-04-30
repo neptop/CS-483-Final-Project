d327f8fafb6aa75f86a4eefad12fa2544979fd58
D925301 <davidhoang777@gmail.com>
2025-04-30 15:10:25 -0700

    Update LoginButton.jsx

---
+++ thoughtstream-frontend/src/components/LoginButton.jsx
@@ -0,0 +1,35 @@
+import { GoogleLogin } from "@react-oauth/google";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import api from "../services/api";
+import { log } from "console";
+import { useNavigate } from "react-router-dom";
+
+function LoginButton() {
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLoginSuccess = async (credentialResponse) => {
+        try {
+            const idToken = credentialResponse.credential;
+
+            // sends the token to backend
+            const res = await api.post("/auth/google", {credential:idToken});
+
+            // login then redirect
+            login(res.data.token, res.data.user);
+            navigate("/dashboard");
+        } catch (err) {
+            console.error("Login failed:", err);
+        }
+    };
+
+    return (
+        <GoogleLogin
+            onSuccess={handleLoginSucess}
+            onError={() => console.error("google login error")}
+        />
+    );
+}
+
+export default LoginButton;
