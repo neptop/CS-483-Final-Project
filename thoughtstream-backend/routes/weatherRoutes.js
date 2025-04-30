e8ecd06d1b70af0cf40b5d9770e46fa85a0bb9ac
D925301 <davidhoang777@gmail.com>
2025-04-30 13:22:23 -0700

    fixed import paths
    fixed import paths for
    - authRoutes.js
    - diaryRoutes.js
    - weatherRoutes.js

--- thoughtstream-backend/routes/weatherRoutes.js
+++ thoughtstream-backend/routes/weatherRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getWeather } from "../../controllers/weatherController.js";
+import { getWeather } from "../controllers/weatherController.js";
 
 const router = express.Router();
 
