7fbff3aa590dc4dfd5e202bf17fe891a7e9f483f
D925301 <davidhoang777@gmail.com>
2025-04-29 18:02:22 -0700

    Update Dashboard.jsx

---
+++ thoughtstream-frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import Header from "../components/Header";
+import WeatherWidget from ".../components/WeatherWidget"
+import NewEntryForm from "../components/NewEntryForm"
+import DiaryList from "../components/DiaryList"
+
+function Dashboard() {
+    const { user } = useContext(AuthContext);
+
+    return (
+        <div className="dashboard-page">
+            <Header />
+            <div className="dashboard-content">
+                <h2>Welcome, {user?.name}!</h2>
+                <WeatherWidget />
+                <NewEntryForm />
+                <DiaryList />
+
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard;
